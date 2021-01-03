6d0a9447898e99f9e7c2bd1f8417a8c473006ff2
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2021-01-03 19:36:00 +0330

    define amplitude keys

--- Watchlitude/AmplitudeKeys.swift
+++ Watchlitude/AmplitudeKeys.swift
@@ -5,4 +5,31 @@
 //  Created by Mohammad Rahchamani on ۱۳۹۹/۱۰/۱۲.
 //
 
-import Foundation
+enum AmplitudeKeys: String {
+    case apiKey = "api_key"
+    
+    case platform = "platform"
+    case osName = "os_name"
+    case osVersion = "os_version"
+    
+    case deviceManufacturer = "device_manufacturer"
+    case deviceModel = "device_model"
+    case deviceId = "device_id"
+    
+    case userId = "user_id"
+    
+    case idfa = "idfa"
+    case idfv = "idfv"
+    
+    case appVersion = "app_version"
+    
+    case sessionId = "session_id"
+    
+    case userProperties = "user_properties"
+    
+    case time = "time"
+    
+    case events = "events"
+    case eventType = "event_type"
+    case eventProperties = "event_properties"
+}
