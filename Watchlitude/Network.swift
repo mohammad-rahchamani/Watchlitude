16e725f40169ecb1e922fc9f9a9053e2ee84eaae
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2021-01-03 19:36:38 +0330

    send request to amplitude api

--- Watchlitude/Network.swift
+++ Watchlitude/Network.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class Network {
+    
+    private static let AMPLITUDE_URL = URL(string: "https://api.amplitude.com/2/httpapi")!
+    
+    static func sendRequest(_ data: [String: Any]) {
+        
+        guard let body = try? JSONSerialization.data(withJSONObject: data,
+                                                     options: []) else {
+            print("unable to serialize to json.")
+            return
+        }
+        
+        let session = URLSession(configuration: .default)
+        
+        var request = URLRequest(url: AMPLITUDE_URL)
+        request.httpMethod = "POST"
+        
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Content-type")
+        
+        
+        request.httpBody = body
+        
+        let task = session.dataTask(with: request)
+        
+        task.resume()
+        
+    }
+    
+}
